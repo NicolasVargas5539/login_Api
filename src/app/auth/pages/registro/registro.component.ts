5e7b3767fef7e9b24666d9d3eb52cbc0bd3f367e
Nicolas Vargas <nicolas.vargas1085@gmail.com>
2023-01-09 20:35:48 -0500

    Login-Registro

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { DatosService } from '../../../protected/services/datos.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registro',
@@ -18,4 +22,32 @@ import { Component } from '@angular/core';
 })
 export class RegistroComponent {
 
+  constructor(private formBuilder: FormBuilder,
+            private datosService: DatosService,
+            private router: Router,
+            private toastr: ToastrService,){}
+
+
+  editarUsuario: FormGroup = this.formBuilder.group({
+    nombre: ['', [Validators.required]],
+    correo: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    rol: ['ADMIN_ROLE', [Validators.required]],
+  })
+
+  createUser(){
+    const { nombre, password, correo, rol } = this.editarUsuario.value;
+
+    this.datosService.insertarUsuario(nombre, password, correo, rol)
+      .subscribe(ok => {
+        console.log(ok.usuario?.estado)
+        if (ok.usuario?.estado === true) {
+          this.router.navigateByUrl('/inicio/usuarios')
+          this.toastr.success('Genial!', 'Usuario Creado Correctamente!');
+        }
+        // else {
+          // this.toastr.error("El formulario no pudo ser procesado", 'Error!');
+        // }
+      })
+  }
 }
